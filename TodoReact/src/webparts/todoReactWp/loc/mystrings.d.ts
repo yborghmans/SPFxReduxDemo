6de7caccaaae5259837468b33d20f75ce4a85d57
Yannick Borghmans <yannick.borghmans@outlook.be>
2019-05-06 12:17:00 +0200

    initial generator files

---
+++ TodoReact/src/webparts/todoReactWp/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ITodoReactWpWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'TodoReactWpWebPartStrings' {
+  const strings: ITodoReactWpWebPartStrings;
+  export = strings;
+}
